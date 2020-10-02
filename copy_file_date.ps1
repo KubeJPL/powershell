472189db74e9935435f3d5aef803eda2a86ccef8
Marius <marius.xmp@gmail.com>
2020-10-02 10:10:44 +0300

    Modified script

---
+++ copy_file_date.ps1
@@ -0,0 +1,25 @@
+
+
+$file_name_date = $args[0]
+
+$source_ip = @('10.0.2.4', '10.0.2.5')
+
+$file_name = "webportal.log"
+
+if ($file_name_date){
+
+  $file_name = "$($file_name).$($file_name_date)"
+
+}
+
+
+$source_path = "\c$\Users\Administrator\Desktop\share_fold\$($file_name)"
+
+$destination_path = "C:\Users\Administrator\Desktop\"
+
+
+foreach ($i in $source_ip) {
+$f_name = $i
+    Copy-Item -Path \\"$($i)$($source_path)" -Destination "$($destination_path)$($i)_$($file_name)"
+    echo "File $($file_name) from $($i) was copied to $($destination_path)$($i)_$($file_name)"
+    }
