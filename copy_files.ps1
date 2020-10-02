29461b9590ce8af60e1e908737b7a73599791dde
Marius <marius.xmp@gmail.com>
2020-10-02 09:45:28 +0300

    added copy_file_date file

--- copy_files.ps1
+++ copy_files.ps1
@@ -3,6 +3,12 @@ $source_ip = @('10.0.2.4', '10.0.2.5')
 
 $file_name = "webportal.log"
 
+$file_name_date = $args[0]
+
+if (!$file_name_date){
+  $file_name = "webportal.log.$($file_name_date)"
+}
+
 $source_path = "\c$\Users\Administrator\Desktop\share_fold\$($file_name)"
 
 $destination_path = "C:\Users\Administrator\Desktop\"
