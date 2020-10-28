c40c7750082219fbf2642ecdd4e83b430fe64120
Marius <72443848+marius-madalin@users.noreply.github.com>
2020-10-28 15:00:53 +0200

    Add files via upload

---
+++ backup_server.ps1
@@ -0,0 +1,48 @@
+$date = Get-Date -Format 'yyyy/MM/dd'
+
+$server_source_name = "ADRIAN\SQLEXPRESS"
+
+$destinationdatabase = "AdventureWorks2012"
+
+$backup_dest = "C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\Backup\$destinationdatabase-$date"
+
+$backup_source = "C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\Backup\$destinationdatabase-$date"
+
+
+
+
+# Script for backup
+
+$scriptForBackup="BACKUP DATABASE $destinationdatabase TO  DISK = N'$backup_dest' WITH NOFORMAT, NOINIT,  NAME = N'$backup_dest);', SKIP, NOREWIND, NOUNLOAD, NO_COMPRESSION,  STATS = 10
+GO"
+
+# Script for restore
+
+$scriptForRestore="USE [master]
+ALTER DATABASE $destinationdatabase SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+RESTORE DATABASE $destinationdatabase FROM  DISK = N'$backup_source' WITH  FILE = 2,  NOUNLOAD,  REPLACE,  STATS = 5
+ALTER DATABASE $destinationdatabase SET MULTI_USER
+
+GO"
+
+
+
+# Execution
+
+
+if($args[0] -eq "Backup"){
+
+Write-Host "We are backing-up $destinationdatabase"
+
+$Backup = Invoke-Sqlcmd -ServerInstance "$server_source_name" -Query $scriptForBackup -Database master -Verbose -ErrorAction stop
+
+}
+
+elseif($args[0] -eq "Restore"){
+
+Write-Host "We are restoring $destinationdatabase"
+
+$Restore = Invoke-Sqlcmd -ServerInstance "$server_source_name" -Query $scriptForRestore -Database master -Verbose -ErrorAction stop
+
+}
+
